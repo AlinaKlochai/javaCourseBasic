a4c76e1d8374ec3fa9594726546f65f86378243f
AlinaKlochai <mrs.ali@tuta.io>
2024-01-17 17:46:34 +0100

    homework1

---
+++ src/homeworks/Homework02.java
@@ -0,0 +1,43 @@
+package homeworks;
+
+public class Homework02 {
+
+    public static void main(String[] args) {
+
+        // Task 2
+        System.out.println("-----Task 2-----");
+
+
+        char a = 'A';
+        System.out.println("char: " + a);
+
+        int b = 96;
+        System.out.println("int: " + b);
+
+        byte c = 8;
+        System.out.println("byte: " + c);
+
+        short d = 65;
+        System.out.println("short: " + d);
+
+        float e = 6.4F;
+        System.out.println("float: " + e);
+
+        double f = 3.769420413;
+        System.out.println("double: " + f);
+
+        long g = 14264;
+        System.out.println("long: " + g);
+
+        // Task 3
+        System.out.println("-----Task 3-----");
+
+        int x = 567;
+
+        System.out.println(x / 100 + ",");
+        System.out.println((x - 500 - 7) / 10 + ",");
+        System.out.println(x - 500 - 60);
+
+
+    }
+}

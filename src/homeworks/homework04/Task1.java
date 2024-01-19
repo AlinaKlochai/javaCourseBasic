f7f070f900f37d360d71897b64b30bc670ac8f32
AlinaKlochai <mrs.ali@tuta.io>
2024-01-19 16:05:25 +0100

    homework4 task 1

---
+++ src/homeworks/homework04/Task1.java
@@ -0,0 +1,7 @@
+package homeworks.homework04;
+
+public class Task1 {
+    String name;
+    int age;
+    String country;
+}
---
+++ src/homeworks/homework04/Task1Demo.java
@@ -0,0 +1,13 @@
+package homeworks.homework04;
+
+public class Task1Demo {
+    public static void main(String[] args) {
+        String name = "Алина Клочай";
+        int age = 27;
+        String country = "Украины";
+
+        System.out.println("Меня зовут " + name + ".");
+        System.out.println("Мне " + age + " лет" + ".");
+        System.out.println("Я из " + country + ".");
+    }
+}

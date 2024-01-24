520fae20765a53d94451816265c5444900e5e186
AlinaKlochai <mrs.ali@tuta.io>
2024-01-24 09:25:28 +0100

    homework6

--- src/homeworks/homework06/StringDemo.java
+++ src/homeworks/homework06/StringDemo.java
@@ -4,7 +4,7 @@ public class StringDemo {
     public static void main(String[] args) {
 
         String myStringAboutMe = new String();
-        myStringAboutMe.stringAboutMe1 = "I study Basic Java!";
+        myStringAboutMe.stringAboutMe = "I study Basic Java!";
 
     }
 

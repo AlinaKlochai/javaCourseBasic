c5e8ffa7f35734343bb30b65cfe89428f8bf2a88
AlinaKlochai <mrs.ali@tuta.io>
2024-01-24 17:40:30 +0100

    extraHomework2 task 1

---
+++ src/homeworks/extraHomework/Robot.java
@@ -0,0 +1,22 @@
+package homeworks.extraHomework;
+
+public class Robot {
+
+    //Создайте в классе Robot метод со следующей сигнатурой:
+    public void sayHello(){
+        //тут напишите команду распечатки на консоли строки "Hello!"
+        System.out.println("Hello!");
+    }
+
+    //В классе робот (Robot) создайте конструктор без параметров:
+
+    public Robot() {
+
+    }
+    //Создайте в классе Robot метод со следующей сигнатурой:
+
+    public void sayYourName(){
+        System.out.println("My name is ROBO");
+    }
+
+}
---
+++ src/homeworks/extraHomework/RobotDemo.java
@@ -0,0 +1,20 @@
+package homeworks.extraHomework;
+
+public class RobotDemo {
+
+    //Внутри класса RobotDemo создайте метод
+    public static void main(String[] args) {
+
+        //создайте экземпляр класса Robot вызвав конструктор класса
+
+        Robot robot1 = new Robot();
+        //В методе main(...) класса RobotDemo вызовите метод sayHello() у созданного ранее виртуального обьекта.
+        robot1.sayHello();
+
+        //В методе main(...) класса RobotDemo вызовите метод sayYourName() у созданного ранее обьекта.
+
+        Robot robot2 = new Robot();
+        robot2.sayYourName();
+
+    }
+}

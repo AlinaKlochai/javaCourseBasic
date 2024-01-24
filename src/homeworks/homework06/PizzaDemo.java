0db9d382181a70b14fb5357a039159253e1698fa
AlinaKlochai <mrs.ali@tuta.io>
2024-01-24 17:21:42 +0100

    homework6 task 3

---
+++ src/homeworks/homework06/PizzaDemo.java
@@ -0,0 +1,34 @@
+package homeworks.homework06;
+
+   /*
+
+Напишите программу, которая вычисляет, сколько лишних калорий будет,
+если вместо пиццы диаметром 24 см вы купите пиццу диаметром 28 см.
+Чтобы решить эту проблему, предположим, что каждый квадратный сантиметр пиццы содержит 40 калорий.
+
+
+    1. считаем площадь в пицце № 1
+
+    2. считаем площадь в пицце № 2
+
+    3. считаем количество калорий в пицце № 1
+
+    4. считаем количество калорий в пицце № 2
+
+     5. находим разницу
+
+     */
+
+public class PizzaDemo {
+    public static void main(String[] args) {
+        int diameterPizza1 = 24;
+        int diameterPizza2 = 28;
+        int caloriesInOneCm = 40;
+
+        Pizza pizza1 = new Pizza(diameterPizza1, caloriesInOneCm);
+        Pizza pizza2 = new Pizza(diameterPizza2, caloriesInOneCm);
+        double extraCalories = pizza2.totalCaloriesInPizza() - pizza1.totalCaloriesInPizza();
+        System.out.println("Количество лишних калорий: " + extraCalories);
+
+    }
+}

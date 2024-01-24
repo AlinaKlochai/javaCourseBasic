11b24cc3eda4317f696dc6b8a739f94fe74af5c9
AlinaKlochai <mrs.ali@tuta.io>
2024-01-24 20:35:58 +0100

    Homework6 task 4

--- src/homeworks/homework06/UserInputDemo.java
+++ src/homeworks/homework06/UserInputDemo.java
@@ -2,6 +2,19 @@ package homeworks.homework06;
 
 public class UserInputDemo {
     public static void main(String[] args) {
+        //Тема: создание переменной типа String.
+        //Напишите программу в которой создайте переменную
+        //типа String и присвойте ей в качестве начального значения
+        //ваше имя. Выведете на консоль значение этой переменной.
+        String myName  = "Alina";
+        System.out.println(myName);
+
+        //Тема: Конкатенация строк.
+        //Напишите программу в которой создайте переменную
+        //типа String и присвойте ей в качестве начального значения
+        //ваше имя. Выведете на консоль приветствие типа "Hi YourName".
+        System.out.println("Hi " + myName + " !");
+
         UserInput ui = new UserInput();
 
         String name = ui.inputName("Введите имя: ");

e2c9b6b5f76f8a48fbf7e5697734de4b831d5e3e
AlinaKlochai <mrs.ali@tuta.io>
2024-01-28 15:44:56 +0100

    Homework9 task 1

---
+++ src/homeworks/homework09/task1_1.java
@@ -0,0 +1,25 @@
+package homeworks.homework09;
+
+import scanner.UserInput;
+
+public class task1_1 {
+
+    //Необходимо написать программу, где бы пользователю предлагалось ввести число на выбор: 1, 2 или 3,
+    //а программа должна сказать, какое число ввёл пользователь: 1, 2, или 3
+    public static void main(String[] args) {
+
+        UserInput ui = new UserInput();
+
+        int x = ui.inputInteger("Введите число: ");
+
+        if (x == 1){
+            System.out.println("Вы ввели число 1.");
+        } else if ( x== 2){
+            System.out.println("Вы ввели число 2.");
+        } else if ( x == 3){
+            System.out.println("Вы ввели число 3.");
+        }
+
+    }
+
+}
---
+++ src/homeworks/homework09/task1_2.java
@@ -0,0 +1,30 @@
+package homeworks.homework09;
+
+import scanner.UserInput;
+
+public class task1_2 {
+
+    //Необходимо написать программу, где бы пользователю предлагалось ввести число на выбор: 1, 2 или 3,
+    //а программа должна сказать, какое число ввёл пользователь: 1, 2, или 3 с использованием switch
+    public static void main(String[] args) {
+        UserInput ui = new UserInput();
+        int x = ui.inputInteger("Введите число: ");
+
+        switch (x){
+            case 1:
+                System.out.println("Вы ввели число 1.");
+                break;
+            case 2:
+                System.out.println("Вы ввели число 2.");
+                break;
+            case 3:
+                System.out.println("Вы ввели число 3.");
+                break;
+            default:
+                System.out.println("Этого числа нет в моей базе данных :(((");
+
+        }
+
+    }
+
+}

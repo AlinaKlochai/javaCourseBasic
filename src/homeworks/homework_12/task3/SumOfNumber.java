aca3d016d8fae63dbe74b422ba50942acb89b428
AlinaKlochai <mrs.ali@tuta.io>
2024-01-31 14:10:11 +0100

    homework 12 task3

---
+++ src/homeworks/homework_12/task3/SumOfNumber.java
@@ -0,0 +1,44 @@
+package homeworks.homework_12.task3;
+
+import java.util.Scanner;
+
+public class SumOfNumber {
+
+    /*
+    Задание 2.
+    Напишите программу, которая запрашивает у пользователя несколько чисел
+    при вводе 0 - печатает сумму всех введенных чисел и их среднее арифметическое значение
+
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+
+        boolean condition = true;
+
+        System.out.println("Please enter integer number (0 for exit");
+
+        while (condition){
+            int number = scanner.nextInt();
+
+            if (number == 0){
+                condition = false;
+
+
+            }else {
+                System.out.println("Please enter next integer number");
+
+
+                sum = sum + number;
+                System.out.println("Sum all number is: " + sum);
+
+
+
+
+            }
+
+
+        }
+
+    }
+}

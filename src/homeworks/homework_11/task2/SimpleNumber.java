c01342821a781dd97ae5258e6c110babf7fa6732
AlinaKlochai <mrs.ali@tuta.io>
2024-01-31 14:16:55 +0100

    homework 11 task2

---
+++ src/homeworks/homework_11/task2/SimpleNumber.java
@@ -0,0 +1,40 @@
+package homeworks.homework_11.task2;
+
+import java.util.Scanner;
+
+public class SimpleNumber {
+
+    /*проверить является ли число которое ввел пользователь - простым
+   (делится без остатка только на 1 и на себя)
+     */
+
+    public static void main(String[] args) {
+
+        // запрашиваем у пользователя число
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите число: ");
+        int number = scanner.nextInt();
+
+        // Проверяем является ли введенное число пользователем ПРОСТЫМ
+
+        /*
+        boolean isPrime = true;
+
+        if ((condition1 == 0) && (condition2 == 0)){
+            System.out.println(number + " - это число простое");
+        }
+
+         */
+
+
+
+
+
+    }
+
+
+
+
+}

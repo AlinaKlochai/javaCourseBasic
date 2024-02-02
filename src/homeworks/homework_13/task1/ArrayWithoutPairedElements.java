5dee2f382646b26eb5f3b667a2e5f134225c6309
AlinaKlochai <mrs.ali@tuta.io>
2024-02-02 09:41:21 +0100

    homework 13 task1

--- src/homeworks/homework_13/task1/ArrayWithoutPairedElements.java
+++ src/homeworks/homework_13/task1/ArrayWithoutPairedElements.java
@@ -21,7 +21,7 @@ public class ArrayWithoutPairedElements {
         int[] array = new int[8];
 
         int min = 1;
-        int max = 49;
+        int max = 50;
 
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * ( max - min + 1) + min);

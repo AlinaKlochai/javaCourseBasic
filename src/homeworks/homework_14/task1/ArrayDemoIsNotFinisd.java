c671986ec8bdb44d9db71a0864243f0c955a8a52
AlinaKlochai <mrs.ali@tuta.io>
2024-02-06 19:44:47 +0100

    homework 14 task1

--- src/homeworks/homework_14/task1/ArrayDemoIsNotFinisd.java
+++ src/homeworks/homework_14/task1/ArrayDemoIsNotFinisd.java
@@ -1,5 +1,7 @@
 package homeworks.homework_14.task1;
 
+import java.util.Arrays;
+
 public class ArrayDemoIsNotFinisd {
     public static void main(String[] args) {
         /*Дан массив размера  n-1, содержащий только различные целые числа в диапазоне от 1 до n .
@@ -13,6 +15,26 @@ public class ArrayDemoIsNotFinisd {
         out: 9
          */
 
+        int[] array = {6,1,2,8,3,4,7,10,5};
+        System.out.println("Наш исходный массив: ");
+        System.out.println(Arrays.toString(array));
+
+        //сортируем исходный массив вложенным методом сортировки
+        Arrays.sort(array);
+        System.out.println("Отсортированный массив по возрастанию: ");
+        System.out.println(Arrays.toString(array));
+
+        //ищем недостающий елемент в массиве
+        int n = array.length;
+        int sumProgression = n * (1 + n)/2;                                   // n * (1 + n)/2;
+        int sumArray = 0;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            sumArray = sumArray + array[i];
+        }
+        int missedNumber = sumProgression - sumArray;
+
+        System.out.println("Наш пропущенный елемент: " + missedNumber);
 
     }
 }

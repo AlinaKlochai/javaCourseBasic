a96597b851998f24ef769e38145ae3763f44fabb
AlinaKlochai <mrs.ali@tuta.io>
2024-02-06 22:18:48 +0100

    extraHomework 2 task11

---
+++ src/homeworks/extraHomework/addHomework2/task11/Robot.java
@@ -0,0 +1,54 @@
+package homeworks.extraHomework.addHomework2.task11;
+
+public class Robot {
+    public void sayHello(){
+        System.out.println("Hello!");
+    }
+
+        /*Дайте роботу имя!
+
+            Тема: объявление свойства класса.
+
+            Мы можем создать сколько угодно виртуальных обьектов робот.
+            Но все они при вызове метода sayYourName() печатают на
+            консоль одно и то же: "My name is ROBO". Давайте это исправим!
+            Давайте научим наших роботов запоминать своё уникальное имя
+            при создании и потом использовать его при вызове команды
+            sayYourName().
+
+            Добавьте в класс Robot свойство name:
+
+            class Robot {
+
+             // в этом свойстве наш виртуальный обьект робот будет
+             // запоминать своё имя. Мы используем тип String потому
+             // что имя это строка.
+             String name;
+
+                ....
+    }
+
+         */
+        public Robot(){
+
+        }
+        public void sayYourName(){
+            System.out.println("My name is " + name);
+        }
+
+    String name;
+
+    /*
+        // Это конструктор класса. У этого конструктора один параметр
+        // с названием name и типом String. Тоесть при создании робота
+        // в его конструктор будут передавать название робота.
+        Robot(String robotName) {
+            this.name = robotName; // а тут робот запоминает в своем свойстве this.name как его зовут
+        }
+
+     */
+
+    public Robot(String name) {
+        this.name = name;
+    }
+}
---
+++ src/homeworks/extraHomework/addHomework2/task11/RobotDemo.java
@@ -0,0 +1,31 @@
+package homeworks.extraHomework.addHomework2.task11;
+
+
+
+public class RobotDemo {
+    public static void main(String[] args) {
+        /*
+
+        В классе RobotDemo передайте в конструктор каждого робота его название:
+
+        Robot robot1 = new Robot("Rider"); // пусть этого робота зовут "Rider"
+        robot1.sayHello();
+        robot1.sayYourName();
+
+        Robot robot2 = new Robot("John"); // а этого робота зовут "John"
+        robot2.sayHello();
+        robot2.sayYourName();
+         */
+
+        Robot robot1 = new Robot("Rider");
+        robot1.sayHello();
+        robot1.sayYourName();
+
+        System.out.println("----------Next Robot-------------");
+
+        Robot robot2 = new Robot("John");
+        robot2.sayHello();
+        robot2.sayYourName();
+
+    }
+}

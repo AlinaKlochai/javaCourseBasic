b933ef33b6d159a4b54004d2aba2a5209f6deb65
AlinaKlochai <mrs.ali@tuta.io>
2024-02-06 22:42:22 +0100

    extraHomework 2 task 14

---
+++ src/homeworks/extraHomework/addHomework2/task_14/Book.java
@@ -0,0 +1,13 @@
+package homeworks.extraHomework.addHomework2.task_14;
+
+public class Book {
+    String title;
+
+    public Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+}

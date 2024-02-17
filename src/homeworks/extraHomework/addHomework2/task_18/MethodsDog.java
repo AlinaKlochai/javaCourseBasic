47db8911850e932b922cf1a523acdbac4f24c057
AlinaKlochai <mrs.ali@tuta.io>
2024-02-18 00:40:58 +0100

    extraHomework

---
+++ src/homeworks/extraHomework/addHomework2/task_18/MethodsDog.java
@@ -0,0 +1,17 @@
+package homeworks.extraHomework.addHomework2.task_18;
+
+public class MethodsDog {
+
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void voice(String name){
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+    }
+}
---
+++ src/homeworks/extraHomework/addHomework2/task_18/MethodsDogDemo.java
@@ -0,0 +1,8 @@
+package homeworks.extraHomework.addHomework2.task_18;
+
+public class MethodsDogDemo {
+    public static void main(String[] args) {
+        MethodsDog dog = new MethodsDog();
+        dog.voice("Baks");
+    }
+}

be91c9bf6894cd02e975d881aa8055bc6eafb9fe
AlinaKlochai <mrs.ali@tuta.io>
2024-02-18 01:07:49 +0100

    extraHomework

---
+++ src/homeworks/extraHomework/addHomework2/task_19/MethodsDog.java
@@ -0,0 +1,17 @@
+package homeworks.extraHomework.addHomework2.task_19;
+
+public class MethodsDog {
+    private String name;
+    private int age;
+
+    public MethodsDog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void voice(){
+        System.out.println(this.name);
+        System.out.println(this.name);
+        System.out.println(this.name);
+    }
+}
---
+++ src/homeworks/extraHomework/addHomework2/task_19/MethodsDogDemo.java
@@ -0,0 +1,8 @@
+package homeworks.extraHomework.addHomework2.task_19;
+
+public class MethodsDogDemo {
+    public static void main(String[] args) {
+        MethodsDog dog = new MethodsDog("Baks",5);
+        dog.voice();
+    }
+}

094348544a5ba9d53b66254f8ad6565ccc73eabe
AlinaKlochai <mrs.ali@tuta.io>
2024-02-25 12:41:18 +0100

    homework 29

---
+++ src/homeworks/homework_29/task_1/Student.java
@@ -0,0 +1,27 @@
+package homeworks.homework_29.task_1;
+
+public class Student {
+    private String firstName;
+    private String secondName;
+
+    public Student(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                '}';
+    }
+}
---
+++ src/homeworks/homework_29/task_1/StudentGroup.java
@@ -0,0 +1,47 @@
+package homeworks.homework_29.task_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentGroup {
+    public static void main(String[] args) {
+        Student student1 = new Student("Amanda","Redison");
+        Student student2 = new Student("Ralph","Well");
+        Student student3 = new Student("Ben","Hope");
+        Student student4 = new Student("Nana","Kenn");
+        Student student5 = new Student("David","Red");
+
+        //Добавьте в список пять имен студентов.
+
+        List<Student> studentGroup = new ArrayList<>();
+        studentGroup.add(student1);
+        studentGroup.add(student2);
+        studentGroup.add(student3);
+        studentGroup.add(student4);
+        studentGroup.add(student5);
+
+        System.out.println("Список студентов: " );
+        for (Student student : studentGroup ){
+            System.out.println(student.getFirstName() + " " + student.getSecondName());
+        }
+
+        //Выведите список студентов в обратном порядке.
+
+        System.out.println("Список студентов в обратном порядке:");
+
+        for (int i = studentGroup.size() - 1; i >= 0 ; i--) {
+            Student currentStudent = studentGroup.get(i);
+            System.out.println(currentStudent.getFirstName() + " " + currentStudent.getSecondName());
+
+        }
+
+        //Удалите студента из списка по индексу и выведите обновленный список.
+
+        studentGroup.remove(3);
+        System.out.println("Список студентов без одного студента: " );
+        for (Student student : studentGroup ){
+            System.out.println(student.getFirstName() + " " + student.getSecondName());
+        }
+
+    }
+}

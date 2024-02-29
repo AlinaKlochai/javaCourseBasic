b179a762bef405234d8085973cc824ae41e94a7d
AlinaKlochai <mrs.ali@tuta.io>
2024-02-29 20:36:27 +0100

    homework 33

---
+++ src/homeworks/homework_33/task_1/ContactManager.java
@@ -0,0 +1,58 @@
+package homeworks.homework_33.task_1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContactManager {
+    private Set<Contact> contacts = new HashSet<>();
+
+    public void addContact(Contact contact){
+        if (!contacts.contains(contact)){
+            contacts.add(contact);
+            System.out.println(contact.getName() + contact.getPhoneNumber() + "Добавлен");
+        }else{
+            System.out.println("Этот контакт не может быть добавлен");
+        }
+    }
+
+    public void removeContact(Contact contact){
+        if (contacts.contains(contact)){
+            contacts.remove(contact);
+            System.out.println(contact.getName() + contact.getPhoneNumber() + "Удален");
+        }else {
+            System.out.println(contact.getName() + contact.getPhoneNumber() + " не может быть удален");
+        }
+
+    }
+
+    public void getContacts(){
+        System.out.println("Все контакты в телефонной книге: ");
+        for (Contact contact : contacts){
+            System.out.println(contact.getName() + " -> " + contact.getPhoneNumber());
+        }
+    }
+
+    public Contact getContactByName(String name) {
+        for (Contact contact : contacts) {
+            if (contact.getName().equals(name)) {
+                return contact;
+            }
+        }
+        return null;
+    }
+
+    public Contact getContactByPhoneNumber(String phoneNumber){
+        for (Contact contact : contacts){
+            if (contact.getPhoneNumber().equals(phoneNumber)){
+                return contact;
+            }
+        }
+        return null;
+    }
+
+
+
+
+
+
+}
---
+++ src/homeworks/homework_33/task_1/NumbersBook.java
@@ -0,0 +1,31 @@
+package homeworks.homework_33.task_1;
+
+public class NumbersBook {
+    public static void main(String[] args) {
+        ContactManager contactManager = new ContactManager();
+
+        Contact contact1 = new Contact("Tomas","3467892648");
+        Contact contact2 = new Contact("Dona","678926484674");
+        Contact contact3 = new Contact("Tom","34678926948");
+        Contact contact4 = new Contact("Nick","3467892648");
+        Contact contact5 = new Contact("Tomas","3467892648");
+
+        contactManager.addContact(contact1);
+        contactManager.addContact(contact2);
+        contactManager.addContact(contact3);
+        contactManager.addContact(contact4);
+        contactManager.addContact(contact5);
+
+        System.out.println("---------------------");
+        contactManager.getContacts();
+
+        System.out.println("---------------------");
+        contactManager.removeContact(contact1);
+
+        contactManager.getContacts();
+
+        System.out.println("---------------------");
+        System.out.println("Найден введенный контакт");
+        System.out.println(contactManager.getContactByName("Nick"));
+    }
+}

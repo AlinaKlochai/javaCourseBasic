25301b5756bd3195521dde519b096d8cad8702a1
AlinaKlochai <mrs.ali@tuta.io>
2024-03-07 10:49:44 +0100

    lesson 38

--- src/lessons/lesson_38/CarDemo.java
+++ src/lessons/lesson_38/CarDemo.java
@@ -3,8 +3,8 @@ package lessons.lesson_38;
 public class CarDemo {
     public static void main(String[] args) {
         Car car1 = new Car("BMW",4);
-        Car.Door door = car1.new Door(500.5,800);
-        door.isOpen();
-        door.isClose();
+        Car.Door door1 = car1.new Door(500.5,800);
+        door1.isOpen();
+        door1.isClose();
     }
 }

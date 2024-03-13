ec249dce5929836feb2d94a7fc9ba7e6864204df
AlinaKlochai <mrs.ali@tuta.io>
2024-03-13 21:00:42 +0100

    homework 41

---
+++ src/homeworks/homework_41/task_1/SortedListStrings.java
@@ -0,0 +1,25 @@
+package homeworks.homework_41.task_1;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+Задание: Дан список строк.
+Необходимо отфильтровать из этого списка все строки,
+которые начинаются на букву "A" (без учёта регистра), и
+вернуть список этих строк, отсортированный в алфавитном порядке.
+
+ */
+public class SortedListStrings {
+    public static void main(String[] args) {
+
+        List<String>strings = Arrays.asList("apple", "kiwi", "Apple1", "Abonnement","cat");
+
+        List<String> sortedList = strings.stream()
+                .filter(s -> s.toLowerCase().startsWith("a"))
+                .sorted()
+                .toList();
+
+        System.out.println(sortedList);
+    }
+}

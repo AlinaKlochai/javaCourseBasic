b94370785faa0a0df6b7128cd53a7df891dc1e68
AlinaKlochai <mrs.ali@tuta.io>
2024-03-14 21:55:04 +0100

    homework 41

--- src/homeworks/homework_41/task_3/OrderApp.java
+++ src/homeworks/homework_41/task_3/OrderApp.java
@@ -8,22 +8,22 @@ import java.util.stream.Collectors;
 
 public class OrderApp {
     public static void main(String[] args) {
-        Purcase purcase1 = new Purcase("milk",1);
-        Purcase purcase2 = new Purcase("bread",2);
-        Purcase purcase3 = new Purcase("cafe",10);
+        Purcase purcase1 = new Purcase("milk", 1);
+        Purcase purcase2 = new Purcase("bread", 2);
+        Purcase purcase3 = new Purcase("cafe", 10);
 
-        List<Purcase> purcases1 = Arrays.asList(purcase1,purcase2,purcase3);
+        List<Purcase> purcases1 = Arrays.asList(purcase1, purcase2, purcase3);
         Order order1 = new Order(purcases1, 6);
 
-        Purcase purcase4 = new Purcase("ring",700);
-        Purcase purcase5 = new Purcase("pen",600);
-        Purcase purcase6 = new Purcase("cafe",10);
+        Purcase purcase4 = new Purcase("ring", 700);
+        Purcase purcase5 = new Purcase("pen", 600);
+        Purcase purcase6 = new Purcase("cafe", 10);
 
         List<Purcase> purcases2 = Arrays.asList(purcase4, purcase5, purcase6);
         Order order2 = new Order(purcases2, 4);
 
-        Purcase purcase7 = new Purcase("phone",300);
-        Purcase purcase8 = new Purcase("phone 2",500);
+        Purcase purcase7 = new Purcase("phone", 300);
+        Purcase purcase8 = new Purcase("phone 2", 500);
 
         List<Purcase> purcases3 = Arrays.asList(purcase7, purcase8);
         Order order3 = new Order(purcases3, 12);
@@ -41,8 +41,9 @@ public class OrderApp {
                                 .sum())));
 
         System.out.println("Total sum purcases by month");
-        totatlPriceInOneMonth.forEach((month, total) ->
-                System.out.println("Month: " + month + ", Total price: " + total));
+        for (Map.Entry<Integer, Double> entry : totatlPriceInOneMonth.entrySet()) {
+            System.out.println("Month: " + (entry.getKey() + 1) + ", Total price: " + entry.getValue());
 
+        }
     }
 }
